486ee8a63c6e438f236b407f9c382db4d4f1f53d
DSBagan <dsbagan@yandex.ru>
2023-04-21 20:56:36 +0700

    В списке фурнитуры отображается название,

---
+++ MacoApp/CalcList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacoApp
+{
+    class CalcList
+    {
+        public string Articl { get; set; }
+        public string Nam { get; set; }
+        public int Quantit { get; set; }
+    }
+}
--- MacoApp/CalculationWindow.xaml.cs
+++ MacoApp/CalculationWindow.xaml.cs
@@ -1,14 +1,20 @@
-﻿using MaterialDesignColors;
+﻿using Google.Apis.Drive.v3.Data;
+using MaterialDesignColors;
 using MaterialDesignMessageBox;
 using MaterialDesignThemes.Wpf;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Data.Sqlite;
+using Microsoft.Office.Interop.Outlook;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Shapes;
 
@@ -18,19 +24,27 @@ namespace MacoApp
     {
         SqlRequests sqlRequests = new SqlRequests();
         public ObservableCollection<ClassList> ClassLists { get; set; }
-        List<ClassList> ListCalc { get; set; }
+
+        DataTable table1 = new DataTable("Table1"); //Таблица для сохранения расчета
+        DataTable table2 = new DataTable("Table2"); // Таблица для сохранения всех расчетов
 
         int Count = 1;
         string rotation;
         string rotationTwoArg;
         public int quantityBar = 0;
 
-
         public CalculationWindow()
         {
             InitializeComponent();
             Loaded += CalculationWindow_Loaded;
             SaveCalc.IsEnabled = false;
+            table1.Columns.Add(new DataColumn("Артикул", typeof(string)));
+            table1.Columns.Add(new DataColumn("Название", typeof(string)));
+            table1.Columns.Add(new DataColumn("Количество", typeof(int)));
+            
+            table2.Columns.Add(new DataColumn("Артикул", typeof(string)));
+            table2.Columns.Add(new DataColumn("Название", typeof(string)));
+            table2.Columns.Add(new DataColumn("Количество", typeof(int)));
         }
 
         private void CalculationWindow_Loaded(object sender, RoutedEventArgs e)
@@ -57,8 +71,6 @@ namespace MacoApp
                 rotationTwoArg = "Да/Нет";
             }
         }
-        
-
 
         private void ButtonCalc_Click(object sender, RoutedEventArgs e)
         {
@@ -100,16 +112,13 @@ namespace MacoApp
                 {
                     MaterialMessageBox.ShowDialog("Укажите корректное количество комплектов");
                     return;
-                }
-                
+                }     
                
                 queryString = $"Select * from Elements where (Name_Furn like '"+Furn+"') and(System  = 'Не имеет значения' or System  = "+System+") and(Side like 'Не имеет значения' or Side like '"+side+ "') " +
                     "and(Lower_loop like '"+Lower_loop+ "' or Lower_loop like 'Нет') and(Micro_ventilation like '"+ Micro_ventilation + "' or Micro_ventilation like 'Да/Нет')" +
                     "and(Rotation like '"+rotation+ "' or Rotation like '"+rotationTwoArg+"') and(FFH_before = 0 or '" + FFH+"'>=FFH_before) and(FFH_after = 0 or '" + FFH+ "' <= FFH_after)" +
                     " and(FFB_before = 0 or '"+FFB+"'>=FFB_before) and(FFB_after = 0 or '" + FFB+ "' <= FFB_after)";
                 quantityBar = sqlRequests.Que(FFH, FFB); //Вытаскиваем из класса количество ответных планок
-
-
 
                 using (var connection = new SqliteConnection("Data Source=Furnapp.db"))
                 {
@@ -129,7 +138,6 @@ namespace MacoApp
                                 if (reader.GetValue(3).ToString() == "34623" || reader.GetValue(3).ToString() == "34850")
                                 {
                                     collection.Add(new ClassList() { Id = count, Article = "" + reader.GetValue(3).ToString(), Name = "" + reader.GetValue(2).ToString(), Quantity = (int.Parse(reader.GetValue(4).ToString()) * quantity) * quantityBar });
-
                                 }
                                 else
                                 {
@@ -142,7 +150,7 @@ namespace MacoApp
                 }
                 SaveCalc.IsEnabled = true;
             }
-            catch (Exception)
+            catch 
             {
                 //MaterialMessageBox.ShowDialog("Одно или несколько полей пустое");
                 return;
@@ -151,104 +159,106 @@ namespace MacoApp
 
         private void SaveCalc_Click(object sender, RoutedEventArgs e)
         {
-            string queryString;
-            int quantity = Int32.Parse(TextBoxColvo.Text);
-            int System = Int32.Parse(ComboBoxSystem.Text);
-            string side = ComboBoxSide.Text;
-            int FFH = Int32.Parse(TextBoxFFH.Text);
-            int FFB = Int32.Parse(TextBoxFFB.Text);
-            string Lower_loop = ComboBoxLL.Text;
-            string Micro_ventilation = ComboBoxMv.Text;
-            string Furn = ComboBoxFurn.Text;
 
-            string Article;
-            string Name;
-            int Qantity;
+            try
+            {
+                int count = 0;
+                string queryString;
+                string queryStringCreateTable;
+                string Furn = ComboBoxFurn.Text;
+                int quantity = Int32.Parse(TextBoxColvo.Text);
+                int System = Int32.Parse(ComboBoxSystem.Text);
+                string side = ComboBoxSide.Text;
+                int FFH = Int32.Parse(TextBoxFFH.Text);
+                int FFB = Int32.Parse(TextBoxFFB.Text);
+                string Lower_loop = ComboBoxLL.Text;
+                string Micro_ventilation = ComboBoxMv.Text;
 
-            LBListCalc.Items.Add("" + Count + ". " + TextBoxFFB.Text + "/" + TextBoxFFH.Text + ", " + ComboBoxSystem.Text + "я, Откр. " + ComboBoxSide.Text + ", Ниж. петл.- " + ComboBoxLL.Text);
-            Count++;
-            queryString = $"Select * from Elements where (Name_Furn like '" + Furn + "') and(System  = 'Не имеет значения' or System  = " + System + ") and(Side like 'Не имеет значения' or Side like '" + side + "') " +
+                queryString = $"Select * from Elements where (Name_Furn like '" + Furn + "') and(System  = 'Не имеет значения' or System  = " + System + ") and(Side like 'Не имеет значения' or Side like '" + side + "') " +
                     "and(Lower_loop like '" + Lower_loop + "' or Lower_loop like 'Нет') and(Micro_ventilation like '" + Micro_ventilation + "' or Micro_ventilation like 'Да/Нет')" +
                     "and(Rotation like '" + rotation + "' or Rotation like '" + rotationTwoArg + "') and(FFH_before = 0 or '" + FFH + "'>=FFH_before) and(FFH_after = 0 or '" + FFH + "' <= FFH_after)" +
                     " and(FFB_before = 0 or '" + FFB + "'>=FFB_before) and(FFB_after = 0 or '" + FFB + "' <= FFB_after)";
-            quantityBar = sqlRequests.Que(FFH, FFB); //Вытаскиваем из класса количество ответных планок
+                quantityBar = sqlRequests.Que(FFH, FFB); //Вытаскиваем из класса количество ответных планок               
 
-            using (var connection = new SqliteConnection("Data Source=Furnapp.db"))
-            {
-                ObservableCollection<ClassList> collection = null; //Обнуляем коллекцию для нового расчета
-                collection = new ObservableCollection<ClassList>();
-                GridList.ItemsSource = collection;
-                connection.Open();
-                SqliteCommand command = new SqliteCommand(queryString, connection);
-                using (SqliteDataReader reader = command.ExecuteReader())
+                using (var connection = new SqliteConnection("Data Source=Furnapp.db"))
                 {
-                    if (reader.HasRows)
+                    ObservableCollection<ClassList> collection = null; //Обнуляем коллекцию для нового расчета
+                    table1.Rows.Clear();
+                    collection = new ObservableCollection<ClassList>();
+                    GridList.ItemsSource = collection;
+                    connection.Open();
+                    
+                    SqliteCommand command = new SqliteCommand(queryString, connection);
+
+                    using (SqliteDataReader reader = command.ExecuteReader())
                     {
-                        while (reader.Read())
+                        if (reader.HasRows) // если есть данные
                         {
-                            Article = reader.GetValue(3).ToString();
-                            Name = reader.GetValue(2).ToString();
-                            Qantity = (int.Parse(reader.GetValue(4).ToString()));
 
-                            int article = Article.Length;
-                            int name = Name.Length;
-                            //Записываем данные в LBList
-                            if (Article == "34623" || Article == "34850")
+                            while (reader.Read())   // построчно считываем данные
                             {
-                                if (article < 10)
+                                count++;
+                                //Записываем данные в объект класса и ,тем самым, передаём в таблицу
+                                if (reader.GetValue(3).ToString() == "34623" || reader.GetValue(3).ToString() == "34850")
                                 {
-                                    for (int i = 0; i < 10 - article; i++)
-                                    {
-                                        Article += " ";
-                                    }
-                                }
-                                /*if (name > 60)
-                                {
-
+                                    //collection.Add(new ClassList() { Id = count, Article = "" + reader.GetValue(3).ToString(), Name = "" + reader.GetValue(2).ToString(), Quantity = (int.Parse(reader.GetValue(4).ToString()) * quantity) * quantityBar });
+                                    table1.Rows.Add(reader.GetValue(3).ToString(), reader.GetValue(2).ToString(), (int.Parse(reader.GetValue(4).ToString()) * quantity) * quantityBar);
+                            
                                 }
                                 else
                                 {
-                                    for (int i = 0; i < 60-name; i++)
-                                    {
-                                        Name += " ";
-                                    }
-                                }*/
-                                Qantity = (Qantity * quantity) * quantityBar;
-                                LBList.Items.Add("" + Article + "                                                       " + Qantity);
-                            }
-                            else
-                            {
-                                if (article < 10)
-                                {
-                                    for (int i = 0; i < 10 - article; i++)
-                                    {
-                                        Article += " ";
-                                    }
-                                }
-                                /*if (name > 60)
-                                {
+                                    //collection.Add(new ClassList() { Id = count, Article = "" + reader.GetValue(3).ToString(), Name = "" + reader.GetValue(2).ToString(), Quantity = (int.Parse(reader.GetValue(4).ToString()) * quantity) });
 
+                                    table1.Rows.Add(reader.GetValue(3).ToString(), reader.GetValue(2).ToString(), (int.Parse(reader.GetValue(4).ToString()) * quantity));
                                 }
-                                else
-                                {
-                                    for (int i = 0; i < 60 - name; i++)
-                                    {
-                                        Name += " ";
-                                    }
-                                }*/
-                                Qantity = (Qantity * quantity);
-                                LBList.Items.Add("" + Article + "                                                       " + Qantity);
                             }
+                            // Получаем таблицы
+                            SaveTable2();
                         }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                    SaveCalc.IsEnabled = true;
             }
+            catch
+            {
+                //MaterialMessageBox.ShowDialog("Одно или несколько полей пустое");
+                return;
+            }
+
             ButtonSaveTxt.IsEnabled = true;
             SaveCalc.IsEnabled = false;
             TextBoxColvo.Text = "1";
         }
 
+
+        public void SaveTable2()
+        {
+            // Итерируем по строкам таблицы 1
+            foreach (DataRow row1 in table1.Rows)
+            {
+                // Получаем значение первой колонки текущей строки
+                string value1 = row1[0].ToString();
+
+                // Итерируем по строкам таблицы 2
+                DataRow row2 = table2.Rows.Cast<DataRow>().FirstOrDefault(r => r[0].ToString() == value1);
+
+                // Если есть строка в таблице 2 с таким же значением первой колонки
+                if (row2 != null)
+                {
+                    // Складываем значения третьих колонок
+                    int sum = Convert.ToInt32(row1[2]) + Convert.ToInt32(row2[2]);
+
+                    // Присваиваем второй таблице значение суммы 
+                    row2[2] = sum;
+                }
+                else // Если нет строки в таблице 2
+                {
+                    // Добавляем новую строку в таблицу 2 со всеми значениями из таблицы 1
+                    table2.Rows.Add(row1.ItemArray);
+                }
+            }
+        }
 
         //ввод только цифр в текстбоксы
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -275,6 +285,7 @@ namespace MacoApp
             entryiWindow.Show();
             this.Close();
         }
+
 
         //Сохранение расчета в файл
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -316,20 +327,45 @@ namespace MacoApp
                 streamWriter.WriteLine("--------------------------------------------------------------------------------");
                 streamWriter.WriteLine("    Артикул                       Название                      Кол.  Ед.изм.");
                 streamWriter.WriteLine("--------------------------------------------------------------------------------");
-
                 try
                 {
-                    for (int i = 0; i < LBList.Items.Count; i++)
+                    foreach (DataRow row in table2.Rows)
                     {
-                        streamWriter.WriteLine("" + LBList.Items[i]);
+                        string art = Convert.ToString(row["Артикул"]);
+                        string nam = Convert.ToString(row["Название"]);
+                        int qua = Convert.ToInt32(row["Количество"]);
+
+                        if (art.Length < 10)
+                        {
+                            for (int i = 0; i < 10-art.Length; i++)
+                            {
+                                art += " ";
+                            }
+                        }
+                        if (nam.Length < 55)
+                        {
+                            int a = 55-nam.Length;
+                            for (int i = 0; i < a; i++)
+                            {
+                                nam += " ";
+                            }
+                        }
+                        else 
+                        {
+
+                        }
+
+                        streamWriter.WriteLine(art+nam+"   "+qua);
                     }
-                    
+
                     streamWriter.WriteLine("--------------------------------------------------------------------------------");
                     streamWriter.WriteLine();
                     streamWriter.WriteLine("                    Заявку составил________________________");
+
+
                     streamWriter.Close();
                     fs.Close();
-
+                    table2.Rows.Clear();
 
                     MaterialMessageBox.ShowDialog("Файл успешно сохранен");                  
                 }
@@ -341,9 +377,20 @@ namespace MacoApp
                 LBList.Items.Clear();
                 Count = 1;
             }
+            /*using (var connection = new SqliteConnection("Data Source=Furnapp.db"))
+            {
+                connection.Open();
+                string queryStringCreateTable = $"DROP TABLE CALCTABLE";
+                SqliteCommand command1 = new SqliteCommand(queryStringCreateTable, connection);
+                
+                connection.Close();
+            }*/
             ButtonSaveTxt.IsEnabled = false;
         }
 
-        
+        private void SaveDataGridToTxtFile(DataTable table2, string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
